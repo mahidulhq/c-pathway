daa18fee1ac1e89317d2861a27d30e3ba8298f80
kysr <161691748+mahidulhq@users.noreply.github.com>
2025-04-01 00:41:20 +0600

    Create structure.c

---
+++ Intro/structure.c
@@ -0,0 +1,13 @@
+// This is a basic structure of a C programming program:
+
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    float b = 3.14;
+
+    printf("Integer: %d\n", a);
+    printf("Float: %.2f\n", b);
+
+    return 0;
+}
